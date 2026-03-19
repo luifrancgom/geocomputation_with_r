13b8f59eae1722d64ff677c18aecf6ace551f337
luifrancgom <luifrancgom@gmail.com>
2026-03-19 12:18:37 -0500

    adding sf class, sfg and sfc

--- 002_geographic_data_in_r.R
+++ 002_geographic_data_in_r.R
@@ -4,15 +4,15 @@ library(spData)
 library(spDataLarge)
 library(tidyverse)
 
-# sf objects
+# sf objects ----
 class(world)
 names(world)
 
-# Visualization
+# Visualization ----
 plot(world)
 plot(world, max.plot = 9)
 
-# Summary
+# Summary ----
 world["lifeExp"] |>
   summary()
 
@@ -22,7 +22,7 @@ world_mini
 world$geom |>
   class()
 
-# Tidyverse: st_read vs read_sf
+# Tidyverse: st_read vs read_sf ----
 world_dfr <- system.file(
   "shapes/world.gpkg",
   package = "spData"
@@ -38,8 +38,68 @@ world_tbl <- system.file(
 class(world_dfr)
 class(world_tbl)
 
-## Check this
+## Check this ----
 world_tbl |>
   slice_head(n = 2) |>
   select(iso_a2, name_long) |>
   class()
+
+### Basic maps ----
+
+methods(plot)[methods(plot) == "plot.sf"]
+world_asia <- world[world$continent == "Asia", ]
+asia <- st_union(world_asia)
+asia
+plot(asia)
+
+plot(world["continent"], reset = FALSE)
+
+plot(world["continent"], reset = FALSE)
+cex <- sqrt(world$pop) / 10000
+# st_centroid: gives the centroid of a geometry
+# for of_largest = TRUE it tells find the largest
+# landmass (the mainland) and put the point in the
+# center of that
+# Many countries are multipoligons so by default a
+# centroid might end up in the ocean between the
+# mainland and an island
+world_cents <- st_centroid(world, of_largest_polygon = TRUE)
+plot(st_geometry(world_cents), add = TRUE, cex = cex)
+
+world_cents |>
+  glimpse()
+
+india = world[world$name_long == "India", ]
+# st_geometry: in this case the idea is to
+# get the geometry from an sf object.
+plot(st_geometry(india))
+
+plot(
+  st_geometry(india),
+  lwd = 3,
+  col = "gray",
+  # c(bottom, left, top, right)
+  # In that way you can expand the
+  # boundaries of the map
+  ## 0: No extra space at the bottom
+  ## 0.2: Add 20% extra space to the left
+  ## 0.1 :Add 10% extra space to the top
+  ## 1: Add 100% extra space to the right
+  expandBB = c(0, 0.2, 0.1, 1)
+)
+plot(st_geometry(world_asia), add = TRUE)
+
+#### Using ggplot2 ----
+asia |>
+  ggplot() +
+  geom_sf() +
+  geom_sf(
+    data = india,
+    linewidth = 1
+  ) +
+  coord_sf(
+    xlim = c(40, 120),
+    ylim = c(0, 50),
+    # ggplot2 adds about 5% extra space
+    expand = FALSE
+  )
